a37bc6d52c2e426c8e5d21e0e2c9a43d65aed6a3
Enzo Esperguin <enzoesper@hotmail.com>
2019-09-25 23:10:19 -0300

    using inline styles (when ever you want to scope the style and make sure it only applies to one single element )

--- src/App.js
+++ src/App.js
@@ -31,11 +31,22 @@ class App extends React.Component {
   };
 
   render() {
+    const style = {
+      backgroundColor: "white",
+      font: "inherit",
+      border: "1px solid blue",
+      padding: "8px",
+      cursor: "pointer"
+    };
+
     return (
       <div className="App">
         <h1>App component!</h1>
         <p> This is working! </p>
-        <button onClick={this.switchNameHandler.bind(this, "Maximilian")}>
+        <button
+          style={style}
+          onClick={this.switchNameHandler.bind(this, "Maximilian")}
+        >
           Switch Name
         </button>
         {/*}
